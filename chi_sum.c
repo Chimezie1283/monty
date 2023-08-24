37a2fe61fa1a31985b28daf6e40338ea8ddbe51e
chimezie1283 <agbo.chimezie1283@gmail.com>
2023-08-24 09:38:24 +0000

    chi_sum file

---
+++ chi_sum.c
@@ -0,0 +1,33 @@
+#include "monty.h"
+
+/**
+ * chi_sum - This adds the top two elements of the stack.
+ * @head: The stack head
+ * @Kounta: The line number
+ * Return: nothing to return
+*/
+void chi_sum(state_t **head, unsigned int kounta)
+{
+	state_t *j;
+	int lent = 0, auth;
+
+	j = *head;
+	while (j)
+	{
+		j = j->next;
+		lent++;
+	}
+	if (lent < 2)
+	{
+		fprintf(stderr, "L%d: can't add, state too short\n", kounta);
+		fclose(beast.file);
+		free(beast.content);
+		f_state(*head);
+		exit(EXIT_FAILURE);
+	}
+	j = *head;
+	auth = j->n + j->next->n;
+	j->next->n = auth;
+	*head = j->next;
+	free(j);
+}
