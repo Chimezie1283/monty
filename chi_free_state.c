9cae082653587c5ac1566d119c2339851ba7edfb
chimezie1283 <agbo.chimezie1283@gmail.com>
2023-08-24 13:17:07 +0000

    chi_free_state file

---
+++ chi_free_state.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * chi_free_stack - This prints the top
+ * @head: stack head
+ * @kounta: line_number
+ * Return: no return
+*/
+void chi_free_state(state_t **head, unsigned int kounta)
+{
+	(void)head;
+	(void)kounta;
+	beast.lifi = 0;
+}
