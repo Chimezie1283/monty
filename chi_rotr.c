cb73d8b401af8fde859519f8ffc884b198031dc8
chimezie1283 <agbo.chimezie1283@gmail.com>
2023-08-24 13:04:30 +0000

    chi_rotr file

---
+++ chi_rotr.c
@@ -0,0 +1,27 @@
+#include "monty.h"
+
+/**
+  *chi_rotr- This rotates the stack to the bottom
+  *@head: The stack head
+  *@kounta: The line_number
+  *Return: no return
+ */
+void chi_rotr(state_t **head, __attribute__((unused)) unsigned int kounta)
+{
+	state_t *copy;
+
+	copy = *head;
+	if (*head == NULL || (*head)->next == NULL)
+	{
+		return;
+	}
+	while (copy->next)
+	{
+		copy = copy->next;
+	}
+	copy->next = *head;
+	copy->prev->next = NULL;
+	copy->prev = NULL;
+	(*head)->prev = copy;
+	(*head) = copy;
+}
