5847479da02981c2724c44f5ec56dd766a2ccd6c
chimezie1283 <agbo.chimezie1283@gmail.com>
2023-08-24 10:33:23 +0000

    chi_p file

---
+++ chi_p.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+  *chi_p- nothing here to execute
+  *@head: The stack head
+  *@kounta: The line number
+  *Return: nothing to return
+ */
+void chi_p(state_t **head, unsigned int kounta)
+{
+	(void) kounta;
+	(void) head;
+}
