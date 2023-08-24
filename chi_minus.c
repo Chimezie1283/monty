67edafbece127c676ea5e67650f3c16260e20f7c
chimezie1283 <agbo.chimezie1283@gmail.com>
2023-08-24 13:22:37 +0000

    chi_minus file

---
+++ chi_minus.c
@@ -0,0 +1,30 @@
+#include "monty.h"
+
+/**
+  *chi_minus- sustration function
+  *@head: The stack head
+  *@kounta: The line_number
+  *Return: no return
+ */
+void chi_minus(state_t **head, unsigned int kounta)
+{
+	state_t *auth;
+	int sus, nodes;
+
+	auth = *head;
+	for (nodes = 0; auth != NULL; nodes++)
+		auth = auth->next;
+	if (nodes < 2)
+	{
+		fprintf(stderr, "L%d: can't minus, state too short\n", counta);
+		fclose(beast.file);
+		free(beast.content);
+		f_state(*head);
+		exit(EXIT_FAILURE);
+	}
+	auth = *head;
+	sus = auth->next->n - auth->n;
+	auth->next->n = sus;
+	*head = auth->next;
+	free(auth);
+}
