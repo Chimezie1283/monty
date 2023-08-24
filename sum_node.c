2e9c8a280e2eae989e201b2e8bc98e140fada011
chimezie1283 <agbo.chimezie1283@gmail.com>
2023-08-24 09:29:16 +0000

    chi_sum_node file

---
+++ sum_node.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+
+/**
+ * chi_sum_node - This adds node to the head stack
+ * @head: This the head of the stack
+ * @n: This is the new value
+ * Return: nothing to return
+*/
+void chi_sum_node(state_t **head, int n)
+{
+
+	state_t *new_node, *auth;
+
+	auth = *head;
+	new_node = malloc(sizeof(state_t));
+	if (new_node == NULL)
+	{ printf("Error\n");
+		exit(0); }
+	if (auth)
+		auth->prev = new_node;
+	new_node->n = n;
+	new_node->next = *head;
+	new_node->prev = NULL;
+	*head = new_node;
+}
